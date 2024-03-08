ff034ea08b6b8681ad0caee3049f3b98d01050c7
adamd <adam.d.hinton@gmail.com>
2024-03-07 22:24:17 -0500

    Understanding Arrays.sort() under the hood

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,7 +1,30 @@
 package dev.lpa;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
+        Integer five = 5;
+        Integer[] others = {0, 5, 10, -50, 50};
+
+        for(Integer i : others){
+            int val = five.compareTo(i);
+            System.out.printf("%d %s %d: compareTo = %d%n", five, (val == 0 ? "==" : (val < 0) ? "<" : ">"), i, val);
+        }
+
+        String banana = "banana";
+        String[] fruit = {"apple", "banana", "pear", "BANANA"};
+
+        for (String s : fruit){
+            int val = banana.compareTo(s);
+
+            // doesn't just print -1, 0 or 1
+            System.out.printf("%s %s %s: compareTo = %d%n", banana, (val == 0 ? "==" : (val < 0) ? "<" : ">"), s, val);
+        }
+
+        Arrays.sort(fruit);
+        System.out.println(Arrays.toString(fruit));
 
     }
 }
